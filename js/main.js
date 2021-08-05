7e14e16c0c8c5972d5692702feba6761b7cf707d
Zenky <bzenky@gmail.com>
2021-08-05 17:35:44 -0300

    index html css update

--- js/main.js
+++ js/main.js
@@ -1,10 +1,33 @@
 //Menu Hambúrguer
 
-const hbMenu = document.querySelector('.hbMenu')
+// const hbMenu = document.querySelector('.hbMenu')
 
-function toggleHbMenu() {
-  const nav = document.querySelector('.nav')
-  nav.classList.toggle('active')
-}
+// function toggleHbMenu() {
+//   const nav = document.querySelector('.nav')
+//   nav.classList.toggle('active')
+// }
 
-hbMenu.addEventListener('click', toggleHbMenu);
+// hbMenu.addEventListener('click', toggleHbMenu)
+
+//swiper
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
